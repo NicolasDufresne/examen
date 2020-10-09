a444572ec1719b8e231739c51d79b9eee5f0725d
Nicolas <nicoladufresne@gmail.com>
2020-10-09 12:05:02 +0200

    edit CommunesController, add POST,PUT,DELETE

--- communes/src/Controller/CommunesController.php
+++ communes/src/Controller/CommunesController.php
@@ -49,7 +49,7 @@ class CommunesController extends AbstractController
      */
     public function jsonCommunes(CommunesRepository $communesRepository, Request $request)
     {
-        $nom = $request->query->get('name');
+        $nom = $request->query->get('nom');
 
         if ($nom === null) {
             $communes = $communesRepository->findAll();
@@ -60,4 +60,112 @@ class CommunesController extends AbstractController
         return JsonResponse::fromJsonString($this->serializeJson($communes));
 
     }
+
+    /**
+     * @Route("json/communes/{nom}", name="communes_nom", methods={"GET"})
+     * @param Communes $communes
+     * @return JsonResponse
+     */
+    public function communesNom(Communes $communes)
+    {
+        return JsonResponse::fromJsonString($this->serializeJson($communes));
+    }
+
+
+
+
+
+    /**
+     * @Route("/communes/json/create", name="communes_create", methods={"POST"})
+     * @param Request $request
+     * @return Response
+     */
+    public function communesCreate(Request $request)
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $communes = new Communes();
+        $communes->setNom($request->request->get("nom", "undefined"))
+            ->setCode($request->request->get("code","undefined"))
+            ->setCodeDepartement($request->request->get("code_departement","undefined"))
+            ->setCodeRegion($request->request->get("code_region","undefined"))
+            ->setCodesPostaux($request->request->get("codes_postaux","undefined"))
+            ->setPopulation($request->request->get("population","undefined"));
+        $entityManager->persist($communes);
+        $entityManager->flush();
+        $response = new Response();
+        $response->setContent("Creation of commune with id " . $communes->getId());
+        return $response;
+    }
+
+    /**
+     * @Route("/communes/json/update", name="communes_update", methods={"PUT"})
+     * @param Request $request
+     * @param CommunesRepository $communesRepository
+     * @return Response
+     */
+    public function communesUpdate(Request $request, CommunesRepository $communesRepository){
+        $entityManager = $this->getDoctrine()->getManager();
+        $data = json_decode(
+            $request->getContent(),
+            true
+        );
+        $response = new Response();
+        if (isset($data['communes_id']) && isset($data['nom'])) {
+            $id = $data['communes_id'];
+            $communes = $communesRepository->find($id);
+            if ($communes === null) {
+                $response->setContent("Cette commune n'existe pas");
+                $response->setStatusCode(Response::HTTP_BAD_REQUEST);
+            } else {
+                $communes->setNom($data['nom']);
+                $entityManager->persist($communes);
+                $entityManager->flush();
+                $response->setContent("Modification de la commune");
+                $response->setStatusCode(Response::HTTP_OK);
+            }
+        }else{
+            $response->setContent("Erreur Bad Request");
+            $response->setStatusCode(Response::HTTP_BAD_REQUEST);
+        }
+        return $response;
+    }
+
+    /**
+     * @Route("/communes/json/delete", name="communes_delete", methods={"DELETE"})
+     * @param Request $request
+     * @param CommunesRepository $communesRepository
+     * @return Response
+     */
+    public function departementDelete(Request $request, CommunesRepository $communesRepository)
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $response = new Response();
+        $data = json_decode(
+            $request->getContent(),
+            true
+        );
+        if (isset($data["communes_id"]))
+        {
+            $communes = $communesRepository->find($data["communes_id"]);
+            if ($communes === null)
+            {
+                $response->setContent("Cette commune n'existe pas");
+                $response->setStatusCode(Response::HTTP_BAD_REQUEST);
+            }
+            else
+            {
+                $entityManager->remove($communes);
+                $entityManager->flush();
+                $response->setContent("Cette commune à été delete");
+                $response->setStatusCode(Response::HTTP_OK);
+            }
+        }
+        else
+        {
+            $response->setContent("L'id n'est pas renseigné");
+            $response->setStatusCode(Response::HTTP_BAD_REQUEST);
+        }
+        return $response;
+    }
+
 }
